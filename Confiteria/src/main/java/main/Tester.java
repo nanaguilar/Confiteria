a205634a237c04632aa657ce97b92da693fef6e2
nanaguilar <darianazel@gmail.com>
2025-05-22 13:46:32 -0600

    Tester

--- Confiteria/src/main/java/main/Tester.java
+++ Confiteria/src/main/java/main/Tester.java
@@ -1,62 +1,49 @@
 package main;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 import modelo.Dulce;
+import servicio.Servicio;
+import servicio.ServicioDulce;
 
 public class Tester {
-
-	private static EntityManager em = null;
-	private static EntityManagerFactory entityManagerFactory = null;
 
 	public static void main(String[] args) {
 		try {
 			System.out.println("Iniciando");
-			startEntityManagerFactory("Confiteria");
+			
+			// Inicializamos el EntityManagerFactory
+			Servicio.startEntityManagerFactory("Confiteria");
 
-			em = entityManagerFactory.createEntityManager();
+			 // Creamos instancia del servicio
+            ServicioDulce servicioDulce = new ServicioDulce();
 
-			em.getTransaction().begin();
+            // Crear 
+            Dulce nuevoDulce = new Dulce();
+            nuevoDulce.setNombre("Kiss");
+            nuevoDulce.setCantidad(12);
+            nuevoDulce.setPrecio(50);
+            servicioDulce.crearDulce(nuevoDulce);
+            System.out.println("Dulce creado: " + nuevoDulce.getNombre());
 
-			Dulce nuevoDulce = new Dulce();
-			nuevoDulce.setNombre("Lindor");
-			nuevoDulce.setCantidad(6);
-			nuevoDulce.setPrecio(2500);
+            // Leer
+            Dulce dulceLeido = servicioDulce.leerDulce(nuevoDulce.getId());
+            System.out.println("Dulce leído: " + dulceLeido.getNombre());
 
-			em.persist(nuevoDulce);
-			em.getTransaction().commit();
+            // Actualizar
+            dulceLeido.setPrecio(100);
+            servicioDulce.actualizarDulce(dulceLeido);
+            System.out.println("Dulce actualizado con precio: " + dulceLeido.getPrecio());
 
-			stopEntityManagerFactory();
-			System.out.println("Finalizado");
+            // Eliminar
+            servicioDulce.eliminarDulce(dulceLeido.getId());
+            System.out.println("Dulce eliminado con ID: " + dulceLeido.getId());
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+            // Cerramos EntityManagerFactory
+            servicio.Servicio.stopEntityManagerFactory();
 
-	}
-
-	public static void startEntityManagerFactory(String persistenceUnit) throws Exception {
-		if (entityManagerFactory == null) {
-			try {
-				entityManagerFactory = Persistence.createEntityManagerFactory(persistenceUnit);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	public static void stopEntityManagerFactory() throws Exception {
-		if (entityManagerFactory != null) {
-			if (entityManagerFactory.isOpen()) {
-				try {
-					entityManagerFactory.close();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-			entityManagerFactory = null;
-		}
-	}
+            System.out.println("Finalizado");
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
